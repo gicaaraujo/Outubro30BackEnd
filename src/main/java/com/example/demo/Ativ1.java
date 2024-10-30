f12fa09f096b26a9a77afe55a38937e9a0681fbd
Gi Araujo <giovanaolivaraujo506@gmail.com>
2024-10-30 16:24:25 -0300

    outubro30

---
+++ src/main/java/com/example/demo/Ativ1.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class Ativ1 {
+	
+	@GetMapping ("/compras")	
+	public String compras() {
+		return "compras.html";
+	}
+	
+	@GetMapping ("/vendas")	
+	public String vendas() {
+		return "vendas.html";
+	}
+
+	@GetMapping ("/estoque")	
+	public String estoque() {
+		return "estoque.html";
+	}
+}
---
+++ src/main/java/com/example/demo/ProjetoAtiv1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAtiv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAtiv1Application.class, args);
+	}
+
+}
